80cfc12350364b3771e355320e9c736e20ad0d12
Andy Borsz and Sara Cavic <andy.borsz+biscuitvile+pizzaworld@gmail.com>
2014-10-26 20:00:37 -0400

    Add FactoryGirl setup to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+require 'factory_girl_rails'
+include FactoryGirl::Syntax::Methods
+
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
